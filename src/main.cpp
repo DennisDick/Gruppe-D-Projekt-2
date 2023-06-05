aafa3f44cfa651f48249571a033b51827c917f0f
DennisDick <122461822+DennisDick@users.noreply.github.com>
2023-06-05 16:30:06 +0000

    necessary commit for pulling repository
    it is an unplanned commit but necessary for pulling the repository; no necessary changes were made

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
-#include <vector>
 #include <memory>
+#include <vector>
+#include <string>
 
 #include "developer.hpp"
 
 auto main() -> int
 {
+    JuniorDeveloper jun_dev("Dennis Dick", "DoubleD");
+    SeniorDeveloper sen_dev("Luca Diener", "LD");
+
     /*
         initializing vector which will be filled with shared pointer each pointing on a child class of Developer
     */
@@ -14,15 +18,14 @@ auto main() -> int
     /*
         initializing shared pointer for Senior and Junior Developer
     */
-    std::shared_ptr<Developer> SeniorDeveloper = std::make_shared<SeniorDeveloper>();
-    std::shared_ptr<Developer> JuniorDeveloper = std::make_shared<JuniorDeveloper>();  
+    std::shared_ptr<Developer> ptr_SeniorDeveloper = std::make_shared<SeniorDeveloper>(sen_dev);
+    std::shared_ptr<Developer> ptr_JuniorDeveloper = std::make_shared<JuniorDeveloper>(jun_dev);
 
     /*
         filling vector with shared pointers
     */
-    vector_developer.push_back(SeniorDeveloper);
-    vector_developer.push_back(JuniorDeveloper);
-
+    vector_developer.push_back(ptr_SeniorDeveloper);
+    vector_developer.push_back(ptr_JuniorDeveloper);
 
     /*
         iterator goes through vector and each object solves a problem
