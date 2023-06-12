a89183d3c8c13a36e8043105fed8e627155f8da6
DennisDick <122461822+DennisDick@users.noreply.github.com>
2023-06-12 14:57:18 +0000

    commenting and formatting our test.cpp file
    I have added some comments to make our test file more understandable. Moreover the document has been formatted with clang format

--- test/test.cpp
+++ test/test.cpp
@@ -1,20 +1,30 @@
 #include "developer.hpp"
 #include "gtest/gtest.h"
 
-
-class DeveloperTest : public ::testing::Test {
-protected:
-        JuniorDeveloper test_junior_developer_ = JuniorDeveloper("Test Junior", "alias junior test");
-        SeniorDeveloper test_senior_developer_ = SeniorDeveloper("Test Senior", "alias senior test");
+class DeveloperTest : public ::testing::Test
+{
+   protected:
+    /*
+        initialzing to test developer with their respective constructor
+    */
+    JuniorDeveloper test_junior_developer_ = JuniorDeveloper("Test Junior", "alias junior test");
+    SeniorDeveloper test_senior_developer_ = SeniorDeveloper("Test Senior", "alias senior test");
 };
 
-TEST_F(DeveloperTest, GetJunior) {
+TEST_F(DeveloperTest, GetJunior)
+{
+    /*
+        testing if getter functions return the right name/alias of test junior developer
+    */
     EXPECT_EQ(test_junior_developer_.get_developer_name(), "Test Junior");
     EXPECT_EQ(test_junior_developer_.get_developer_alias(), "alias junior test");
 }
 
-TEST_F(DeveloperTest, GetSenior) {
+TEST_F(DeveloperTest, GetSenior)
+{
+    /*
+        testing if getter funtions return the right name/alias of test senior developer
+    */
     EXPECT_EQ(test_senior_developer_.get_developer_name(), "Test Senior");
     EXPECT_EQ(test_senior_developer_.get_developer_alias(), "alias senior test");
 }
-
