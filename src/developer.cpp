89eb8199a8df14a836cb7fef746b41dfe244b1fa
DennisDick <122461822+DennisDick@users.noreply.github.com>
2023-06-13 09:08:59 +0000

    corrected derived solve_problem methods
    both derived methods 'solve_problem' have been optimized by not creating a class while using the method

--- src/developer.cpp
+++ src/developer.cpp
@@ -58,11 +58,11 @@ auto operator<<(std::ostream& out, Developer& developer) -> std::ostream&
 void JuniorDeveloper::solve_problem()
 {
     std::cout << *this << "I am a junior developer and i am solving a problem" << std::endl;
-    Developer::drink_coffee();
+    this->drink_coffee();
 }
 
 void SeniorDeveloper::solve_problem()
 {
     std::cout << *this << "I am a senior developer and i am solving a problem" << std::endl;
-    Developer::drink_coffee();
+    this->drink_coffee();
 }
